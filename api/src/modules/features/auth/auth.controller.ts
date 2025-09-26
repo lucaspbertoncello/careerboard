a6aa1721ef39c3741df0e33d694741a5cbbb9a0c
Lucas <lucasbertoncello1@gmail.com>
2025-09-26 16:12:48 -0300

    docs(auth): fix swagger apit tags

--- api/src/modules/features/auth/auth.controller.ts
+++ api/src/modules/features/auth/auth.controller.ts
@@ -3,11 +3,10 @@ import { AuthService } from "./auth.service";
 import { SignupDto } from "./dto/signup.dto";
 import { SigninDto } from "./dto/signin.dto";
 import { Public } from "src/shared/decorators/public.decorator";
-import { ApiBearerAuth, ApiTags } from "@nestjs/swagger";
+import { ApiTags } from "@nestjs/swagger";
 
 @Public()
-@ApiTags("interviews")
-@ApiBearerAuth()
+@ApiTags("auth")
 @Controller("auth")
 export class AuthController {
   constructor(private readonly authService: AuthService) {}

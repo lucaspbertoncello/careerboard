a88da8ca4a573a35eba532765d98d6859c28e3f4
Lucas <lucasbertoncello1@gmail.com>
2025-09-29 17:35:00 -0300

    fix(auth): integrate use-register-form with UI

--- fe/src/views/pages/auth/Register/index.tsx
+++ fe/src/views/pages/auth/Register/index.tsx
@@ -1,10 +1,13 @@
 import { Button } from "@/views/components/button";
 import { Input } from "@/views/components/input";
 import { Link } from "react-router-dom";
+import { useRegisterForm } from "./use-register-form";
 
 export function Register() {
+  const { register, errors, handleSubmit, isPending } = useRegisterForm();
+
   return (
-    <form className="w-[400px]">
+    <form onSubmit={handleSubmit} className="w-[400px]">
       <div className="mb-4">
         <h1 className="text-secondary-foreground text-3xl font-semibold">
           Create an account
@@ -19,12 +22,31 @@ export function Register() {
       </div>
 
       <div className="flex flex-col gap-2">
-        <Input placeholder="Full name" type="text" />
-        <Input placeholder="E-mail" type="email" />
-        <Input placeholder="Password" type="password" />
+        <Input
+          placeholder="Full name"
+          type="text"
+          {...register("name")}
+          error={errors.name?.message}
+        />
+
+        <Input
+          placeholder="E-mail"
+          type="email"
+          {...register("email")}
+          error={errors.email?.message}
+        />
+
+        <Input
+          placeholder="Password"
+          type="password"
+          {...register("password")}
+          error={errors.password?.message}
+        />
       </div>
 
-      <Button className="mt-6">Create account</Button>
+      <Button isLoading={isPending} className="mt-6">
+        Create account
+      </Button>
     </form>
   );
 }

ff1a90fd668823c7210e5484c9dd65d45df7b28c
Lucas <lucasbertoncello1@gmail.com>
2025-10-01 17:42:36 -0300

    feat: add response delay

--- fe/src/app/services/http-client.ts
+++ fe/src/app/services/http-client.ts
@@ -1,4 +1,5 @@
 import axios from "axios";
+import { sleep } from "../utils/sleep";
 
 export const httpClient = axios.create({
   baseURL: import.meta.env.VITE_APP_BASE_URL,
@@ -12,4 +13,9 @@ httpClient.interceptors.request.use((config) => {
   }
 
   return config;
+});
+
+httpClient.interceptors.response.use(async (data) => {
+  await sleep(5000);
+  return data;
 });

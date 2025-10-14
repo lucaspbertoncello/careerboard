4955321a2c1e8ce48c14d88dba8b212f86256116
Lucas <lucasbertoncello1@gmail.com>
2025-10-14 17:44:42 -0300

    feat(interviews): add more info like yearly interviews/last 3 to summary

--- api/src/modules/entities/interviews/interfaces/interviews-summary.interface.ts
+++ api/src/modules/entities/interviews/interfaces/interviews-summary.interface.ts
@@ -1,6 +1,27 @@
-export interface IInterviewsSummary {
+export interface IInterviewsStats {
   total: number;
   approved: number;
   pending: number;
   rejected: number;
+}
+export interface IRecentInterview {
+  id: string;
+  companyName: string;
+  role: string;
+  appliedAt: Date;
+  status: string;
+}
+
+export interface ICurrentYearInterviews {
+  month: string;
+  total: number;
+  approved: number;
+  pending: number;
+  rejected: number;
+}
+
+export interface IInterviewsSummary {
+  interviewsStats: IInterviewsStats;
+  recentInterviews: IRecentInterview[];
+  currentYearInterviews: ICurrentYearInterviews[];
 }
--- api/src/modules/entities/interviews/interviews.service.ts
+++ api/src/modules/entities/interviews/interviews.service.ts
@@ -8,6 +8,7 @@ import { UpdateInterviewDto } from "./dto/update-interview.dto";
 import { ValidateInterviewOwnership } from "./services/validate-interview-ownership.service";
 import { UpdateInterviewStatusDto } from "./dto/update-interview-status.dto";
 import { IInterviewsSummary } from "./interfaces/interviews-summary.interface";
+import { MONTHS } from "src/shared/constants/months";
 
 @Injectable()
 export class InterviewsService implements IInterviewsService {
@@ -26,13 +27,62 @@ export class InterviewsService implements IInterviewsService {
   }
 
   async getInterviewsSummaryByUserId(userId: string): Promise<IInterviewsSummary> {
+    // get all interviews
     const interviews = await this.interviewsRepository.findMany({ where: { userId }, select: { status: true } });
+    // get last 3 interviews
+    const recentInterviews = await this.interviewsRepository.findMany({
+      where: { userId },
+      orderBy: { appliedAt: "desc" },
+      take: 3,
+    });
 
-    const summary: IInterviewsSummary = {
+    const currentYear = new Date().getFullYear();
+    const startOfYear = new Date(Date.UTC(currentYear, 0, 1));
+    const endOfYear = new Date(Date.UTC(currentYear, 11, 31, 23, 59, 59));
+
+    // get all interviews on current year
+    const yearlyInterviews = await this.interviewsRepository.findMany({
+      where: {
+        userId,
+        appliedAt: {
+          gte: startOfYear,
+          lte: endOfYear,
+        },
+      },
+
+      select: {
+        appliedAt: true,
+        status: true,
+      },
+    });
+
+    const currentYearInterviews = MONTHS.map((month, index) => {
+      const monthInterviews = yearlyInterviews.filter((interview) => {
+        const interviewMonth = new Date(interview.appliedAt).getMonth();
+        return interviewMonth === index;
+      });
+
+      return {
+        month: month,
+        total: monthInterviews.length,
+        approved: monthInterviews.filter((i) => i.status === "APPROVED").length,
+        pending: monthInterviews.filter((i) => i.status === "PENDING").length,
+        rejected: monthInterviews.filter((i) => i.status === "REJECTED").length,
+      };
+    });
+
+    const interviewsStats = {
       total: interviews.length,
       approved: interviews.filter((interview) => interview.status === "APPROVED").length,
       pending: interviews.filter((interview) => interview.status === "PENDING").length,
       rejected: interviews.filter((interview) => interview.status === "REJECTED").length,
+    };
+
+    const summary: IInterviewsSummary = {
+      interviewsStats,
+      recentInterviews,
+      // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+      currentYearInterviews: currentYearInterviews as any,
     };
 
     return summary;
